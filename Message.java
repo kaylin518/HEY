ffa5bdec529da8969b25e04c027b80c81518c25a
kaylinvonbergen <kaylinvb@bu.edu>
2023-11-09 05:37:12 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,10 +1,10 @@
 public class Message {
-  public static void main {
+  public static void main(String[] args) {
     int[] link = {104,116,116,112,115,58,47,47,115,105,116,101,115,46,103,111,111,103,108,101,46,99,111,109,47,98,117,46,101,100,117,47,115,112,97,114,107,103,105,116,104,117,98,109,105,99,114,111,99,104,97,108,108,101,110,103,101,47,104,111,109,101};
     System.out.println("hello " + vars.getName() + "!");
     System.out.println(vars.welcome());
     System.out.println("Here is your decoded message! -> " + decode.decoder(link));
     String toOrder = "Spark! Git Microchallenge";
-    System.out.println("The String '" + toOrder + "' ordered based on ASCII values is ->" + "'" order.asciiOrder(toOrder) + "'");
+    System.out.println("The String '" + toOrder + "' ordered based on ASCII values is -> " + "'" + order.asciiOrder(toOrder) + "'");
   }
 }

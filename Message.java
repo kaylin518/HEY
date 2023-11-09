85aa9b4023bb1c62b2859377048da4a6799c611e
kaylinvonbergen <kaylinvb@bu.edu>
2023-11-09 04:01:13 -0500

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,8 @@
+public class Message {
+  public static void main {
+    int[] link = {104,116,116,112,115,58,47,47,115,105,116,101,115,46,103,111,111,103,108,101,46,99,111,109,47,98,117,46,101,100,117,47,115,112,97,114,107,103,105,116,104,117,98,109,105,99,114,111,99,104,97,108,108,101,110,103,101,47,104,111,109,101};
+    System.out.println("hello " + vars.getName() + "!");
+    System.out.println(vars.welcome());
+    System.out.println("Here is your decoded message! -> " + decode.decoder(link));
+  }
+}

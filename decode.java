310e9afa10d148d412fb25525f393bf2555dc04d
kaylin518 <150314918+kaylin518@users.noreply.github.com>
2023-11-09 12:49:37 +0000

    created decode.java

---
+++ decode.java
@@ -0,0 +1,1 @@
+public class decode {public static void main(String[] args) {}public static String decoder(int[] arr) {String result = ""; for (int i = 0; i < arr.length; i++) {result += String.valueOf( (char)arr[i] );}return result;}}
